d2c4a34614f7560d76ef7e71dadef931e82e99ca
Hollie <fangsihan@sihande-macbook-air.local>
2018-10-26 15:40:40 +0800

    testing

---
+++ itineraryPractice/Function.swift
@@ -0,0 +1,12 @@
+//
+//  Function.swift
+//  itineraryPractice
+//
+//  Created by Sihan Fang on 2018/10/26.
+//  Copyright © 2018 Sihan Fang. All rights reserved.
+//
+
+import Foundation
+// try
+
+    
