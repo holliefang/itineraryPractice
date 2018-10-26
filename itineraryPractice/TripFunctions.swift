86965d9bda4ecbd84ea31efe346c89983a18ea98
Hollie <fangsihan@sihande-macbook-air.local>
2018-10-26 19:27:57 +0800

    added model and functions

---
+++ itineraryPractice/TripFunctions.swift
@@ -0,0 +1,27 @@
+//
+//  TripFunctions.swift
+//  itineraryPractice
+//
+//  Created by Sihan Fang on 2018/10/26.
+//  Copyright © 2018 Sihan Fang. All rights reserved.
+//
+
+import Foundation
+
+class TripFunctions {
+    static func createTrip(tripModel: TripModel) {
+        
+    }
+    
+    static func readTrip() {
+        
+    }
+    
+    static func updateTrip(tripModel: TripModel) {
+        
+    }
+    
+    static func deleteTrip(tripModel: TripModel) {
+        
+    }
+}
---
+++ itineraryPractice/TripModel.swift
@@ -0,0 +1,19 @@
+//
+//  TripModel.swift
+//  itineraryPractice
+//
+//  Created by Sihan Fang on 2018/10/26.
+//  Copyright © 2018 Sihan Fang. All rights reserved.
+//
+
+import Foundation
+
+class TripModel {
+    var id: String!
+    var title: String!
+    
+    init(title: String) {
+        id = UUID().uuidString
+        self.title = title
+    }
+}
