ac0873b2fbbcaefd676412cbb7a5e1a45d11a97e
Hollie <fangsihan@sihande-macbook-air.local>
2018-10-28 17:18:54 +0800

    photo adding feature

--- itineraryPractice/Views/AddTripViewController/AddTripViewController.swift
+++ itineraryPractice/Views/AddTripViewController/AddTripViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Sihan Fang. All rights reserved.
 //
 
+import Photos
 import UIKit
 
 class AddTripViewController: UIViewController {
@@ -14,6 +15,7 @@ class AddTripViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var imagaView: UIImageView!
     
     var doneSaving: (() -> ())?
     
@@ -23,6 +25,13 @@ class AddTripViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         titleLabel.font = UIFont(name: Theme.mainFontName, size: 24)
+        imagaView.layer.cornerRadius = 10
+        
+        // Dropshadow on title
+        titleLabel.layer.shadowOpacity = 1
+        titleLabel.layer.shadowColor = UIColor.white.cgColor
+        titleLabel.layer.shadowOffset = CGSize.zero
+        titleLabel.layer.cornerRadius = 5
     }
     
     @IBAction func cancel(_ sender: Any) {
@@ -30,11 +39,91 @@ class AddTripViewController: UIViewController {
     }
     
     @IBAction func save(_ sender: Any) {
-        TripFunctions.createTrip(tripModel: TripModel(title: TripTextField.text!))
+        TripTextField.rightViewMode = .never
+        
+        guard TripTextField.text != "", let newTripName = TripTextField.text else {
+           
+            //show warning icon
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 20))
+            imageView.image = UIImage(named: "icons8-error-48")
+            imageView.contentMode = .scaleAspectFit
+            TripTextField.rightViewMode = .always
+            TripTextField.rightView = imageView
+            
+            //show red background
+//            TripTextField.backgroundColor = #colorLiteral(red: 0.9525896256, green: 0.3187139993, blue: 0.3022343031, alpha: 1)
+            
+            //red boarder
+//            TripTextField.layer.borderColor = UIColor.red.cgColor
+//            TripTextField.layer.borderWidth = 1
+//            TripTextField.layer.cornerRadius = 5
+//
+            TripTextField.placeholder = "Please enter something!"
+            
+            
+            return
+        }
+        TripFunctions.createTrip(tripModel: TripModel(title: newTripName, image: imagaView.image))
         
         if let doneSaving = doneSaving {
             doneSaving()
         }
         dismiss(animated: true)
     }
+    
+    fileprivate func presentPhotoPickerController() {
+        let myPickerController = UIImagePickerController()
+        myPickerController.delegate = self
+        myPickerController.sourceType = .photoLibrary
+        self.present(myPickerController, animated: true)
+    }
+    
+    @IBAction func addPhoto(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            PHPhotoLibrary.requestAuthorization { (status) in
+                switch status {
+                case .authorized:
+                   self.presentPhotoPickerController()
+                case .notDetermined:
+                    if status == PHAuthorizationStatus.authorized {
+                        self.presentPhotoPickerController()
+                    }
+                case .restricted:
+                    let alert = UIAlertController(title: "Photo Library Restricted", message: "Photo Library is restricted and cannnot be accessed", preferredStyle: .alert)
+                    let OKAction = UIAlertAction(title: "OK", style: .default)
+                    alert.addAction(OKAction)
+                    self.present(alert, animated: true)
+                case .denied:
+                    let alert = UIAlertController(title: "Photo Library Denied", message: "Photo Library was previously denied. Please update your Settings.", preferredStyle: .alert)
+                    let goToSettingAction = UIAlertAction(title: "Go to Settings", style: .default, handler: { (action) in
+                        DispatchQueue.main.async {
+                            if let url = URL(string: UIApplication.openSettingsURLString) {
+                            UIApplication.shared.open(url, options: [:])
+                            }
+                            
+                        }
+                    })
+                    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+                    alert.addAction(goToSettingAction)
+                    alert.addAction(cancelAction)
+                    self.present(alert, animated: true)
+
+                }
+            }
+        }
+    }
+    
+    
+}
+
+
+extension AddTripViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            self.imagaView.image = image
+        }
+        
+        dismiss(animated: true)
+    }
+    
 }
--- itineraryPractice/Views/TripsViewController/TripsTableViewCell.swift
+++ itineraryPractice/Views/TripsViewController/TripsTableViewCell.swift
@@ -14,8 +14,8 @@ class TripsTableViewCell: UITableViewCell {
     
     @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var tripImageView: UIImageView!
     
-
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -23,11 +23,13 @@ class TripsTableViewCell: UITableViewCell {
         cardView.addShadowAndRoundedCorner()
         titleLabel.font = UIFont(name: "Righteous-Regular", size: 32)
         cardView.backgroundColor = Theme.accent
+        tripImageView.layer.cornerRadius = cardView.layer.cornerRadius
 
     }
     
     func setup(tripModel: TripModel) {
         titleLabel.text = tripModel.title
+        tripImageView.image = tripModel.image
     }
 
 }
